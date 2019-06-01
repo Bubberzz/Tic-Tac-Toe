db8fb309b3ca822c5cc9a084ce9b1a53f1626b16
Stan Bubbers <stan.bubbers@live.co.uk>
2019-06-01 13:43:44 +0100

    added comments, tidied up code

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,190 +1,157 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TicTacToe
 {
-    class Program
+    internal class Program
     {
-        // the playfield
-        static char[,] numberList = new char[3, 3] { 
-            { '1', '2', '3' }, //Row 0
-            { '4', '5', '6' }, //Row 1
-            { '7', '8', '9' } //Row 2
+        // Creates a three dimensional array - which will be used for the playing field
+        private static char[,] _numberList = new char[3, 3] {
+            { '1', '2', '3' }, // Row 0
+            { '4', '5', '6' }, // Row 1
+            { '7', '8', '9' }  // Row 2
         };
 
-        static int turns = 0;
-        static void Main(string[] args)
+        // Stores turns players taken / number if iterations
+        private static int _turns;
+        // Stores user input, int 1-9
+        private static int _input;
+
+        private static void Main()
         {
-            //Player 1
-            int player = 2;
-            //User input
-            int input = 0;
-            //Checks input
-            bool inputCheck = true;
-                        
-            // Runs code as long as program runs
+            // Player variable, used to switch turns
+            var player = 2;
+            // Set to true when input is an int / breaks do while loop
+            bool inputCheck;
+
             do
             {
-                
-                if (player == 2)
+                // Player alternation switch
+                switch (player)
                 {
-                    player = 1;
-                    CreateXorO('O', input);
+                    case 1:
+                        player = 2;
+                        AddUserInputToArray('X', _input);
+                        break;
+                    case 2:
+                        player = 1;
+                        AddUserInputToArray('O', _input);
+                        break;
                 }
-                    
-                else if (player == 1)
-                {
-                    player = 2;
-                    CreateXorO('X', input);
-                }
+
                 SetField();
 
-                #region
+                #region CheckingForWinner
+                // Checks if there's a winner based on 3 matching combinations of X's or O's
                 char[] playerChars = { 'X', 'O' };
-                foreach (char playerChar in playerChars)
+                foreach (var playerChar in playerChars)
                 {
-                    if (((numberList[0, 0] == playerChar) && (numberList[0, 1] == playerChar) && (numberList[0, 2] == playerChar))
-                        || ((numberList[1, 0] == playerChar) && (numberList[1, 1] == playerChar) && (numberList[1, 2] == playerChar))
-                        || ((numberList[2, 0] == playerChar) && (numberList[2, 1] == playerChar) && (numberList[2, 2] == playerChar))
-                        || ((numberList[0, 0] == playerChar) && (numberList[1, 1] == playerChar) && (numberList[2, 2] == playerChar))
-                        || ((numberList[2, 0] == playerChar) && (numberList[1, 1] == playerChar) && (numberList[0, 2] == playerChar))
-                        || ((numberList[0, 1] == playerChar) && (numberList[1, 1] == playerChar) && (numberList[2, 1] == playerChar))
-                        || ((numberList[0, 0] == playerChar) && (numberList[1, 0] == playerChar) && (numberList[2, 0] == playerChar))
-                        )
+                    if (((_numberList[0, 0] == playerChar) && (_numberList[0, 1] == playerChar) && (_numberList[0, 2] == playerChar))
+                        || ((_numberList[1, 0] == playerChar) && (_numberList[1, 1] == playerChar) && (_numberList[1, 2] == playerChar))
+                        || ((_numberList[2, 0] == playerChar) && (_numberList[2, 1] == playerChar) && (_numberList[2, 2] == playerChar))
+                        || ((_numberList[0, 0] == playerChar) && (_numberList[1, 1] == playerChar) && (_numberList[2, 2] == playerChar))
+                        || ((_numberList[2, 0] == playerChar) && (_numberList[1, 1] == playerChar) && (_numberList[0, 2] == playerChar))
+                        || ((_numberList[0, 1] == playerChar) && (_numberList[1, 1] == playerChar) && (_numberList[2, 1] == playerChar))
+                        || ((_numberList[0, 0] == playerChar) && (_numberList[1, 0] == playerChar) && (_numberList[2, 0] == playerChar))
+                        || ((_numberList[0, 2] == playerChar) && (_numberList[1, 2] == playerChar) && (_numberList[2, 2] == playerChar))
+                       )
                     {
-                        if (playerChar == 'X')
+                        switch (playerChar)
                         {
-                            Console.WriteLine("\nPlayer 2 has won!");
-                        }
-                        else if (playerChar == 'O')
-                        {
-                            Console.WriteLine("\nPlayer 1 has won");
+                            case 'X':
+                                Console.WriteLine("\nPlayer 1 has won!");
+                                break;
+                            case 'O':
+                                Console.WriteLine("\nPlayer 2 has won!");
+                                break;
                         }
                         Console.WriteLine("\nPress any key to reset the game :)");
                         Console.ReadKey();
                         ResetField();
                         break;
-
-                    }
-                    else if (turns == 10)
-                    {
-                        Console.WriteLine("\nIt's a draw\nPress any key to reset");
-                        Console.ReadKey();
-                        ResetField();
-                        break;
                     }
                 }
+                // Checks if all possible turns have been used up, in which case it's a draw 
+                if (_turns == 10)
+                {
+                    Console.WriteLine("\nIt's a draw\n" + "\nPress any key to reset");
+                    Console.ReadKey();
+                    ResetField();
+                }
                 #endregion
-                //Checking the winner
 
-                #region 
-                //Testing field is already taken
+                #region PlayerInput
+                // Game loop, requires player to enter a 1-9 integer, performs validation
                 do
                 {
                     Console.WriteLine("\nPlayer {0}, please choose a number", player);
                     try
                     {
-                        input = Convert.ToInt32(Console.ReadLine());
-
-                    } catch
+                        _input = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
                     {
                         Console.WriteLine("\nPlease enter a number");
                     }
-
-                    if ((input == 1) && (numberList[0, 0] == '1'))
+                    // Uses the inputted integer to validate it's array position, the input variable will be passed to AddUserInputToArray method
+                    if ((_input == 1 && _numberList[0, 0] == '1') || (_input == 2 && _numberList[0, 1] == '2') ||
+                        (_input == 3 && _numberList[0, 2] == '3') || (_input == 4 && _numberList[1, 0] == '4') ||
+                        (_input == 5 && _numberList[1, 1] == '5') || (_input == 6 && _numberList[1, 2] == '6') ||
+                        (_input == 7 && _numberList[2, 0] == '7') || (_input == 8 && _numberList[2, 1] == '8') ||
+                        (_input == 9 && _numberList[2, 2] == '9'))
                     {
                         inputCheck = true;
                     }
-                    else if ((input == 2) && (numberList[0, 1] == '2'))
+                    else
                     {
-                        inputCheck = true;
-                    }
-                    else if ((input == 3) && (numberList[0, 2] == '3'))
-                    {
-                        inputCheck = true;
-                    }
-                    else if ((input == 4) && (numberList[1, 0] == '4'))
-                    {
-                        inputCheck = true;
-                    }
-                    else if ((input == 5) && (numberList[1, 1] == '5'))
-                    {
-                        inputCheck = true;
-                    }
-                    else if ((input == 6) && (numberList[1, 2] == '6'))
-                    {
-                        inputCheck = true;
-                    }
-                    else if ((input == 7) && (numberList[2, 0] == '7'))
-                    {
-                        inputCheck = true;
-                    }
-                    else if ((input == 8) && (numberList[2, 1] == '8'))
-                    {
-                        inputCheck = true;
-                    }
-                    else if ((input == 9) && (numberList[2, 2] == '9'))
-                    {
-                        inputCheck = true;
-                    } else
-                    {
-                        Console.WriteLine("Incorrect input, try again");
+                        Console.Clear();
+                        SetField();
+                        Console.WriteLine("\nIncorrect input, try again");
                         inputCheck = false;
                     }
-                    #endregion
-
                 } while (!inputCheck);
-                                
-                
             } while (inputCheck);
-
-            //char userImput = GetKeyPress("Player 1 goes first. Please type a number:",
-              //                                   new Char[] { '1', '2', '3' });
-            //Console.WriteLine("Player 1 goes first. Please type a number:");
-            //int userInput = Convert.ToInt32(Console.ReadLine());
-                   
-                 
             Console.Read();
         }
+        #endregion
 
+        // Resets the playing field to beginning
         public static void ResetField()
         {
-            char[,] numberListInitial = new char[3, 3] 
+            var numberListInitial = new char[3, 3]
             {
-            { '1', '2', '3' }, //Row 0
-            { '4', '5', '6' }, //Row 1
-            { '7', '8', '9' } //Row 2
+            { '1', '2', '3' }, // Row 0
+            { '4', '5', '6' }, // Row 1
+            { '7', '8', '9' }  // Row 2
              };
-            numberList = numberListInitial;
+            _numberList = numberListInitial;
             SetField();
-            turns = 0;
+            _turns = 1;
+            _input = 0;
         }
+
+        // Draws the playing field - using a multidimensional array to display values
         public static void SetField()
         {
             Console.Clear();
-            Console.WriteLine("   |   |   \n {0} | {1} | {2} \n___|___|___\n   |   |   \n {3} | {4} | {5} \n___|___|___\n   |   |   \n {6} | {7} | {8} \n", numberList[0, 0], numberList[0, 1], numberList[0, 2], numberList[1, 0], numberList[1, 1], numberList[1, 2], numberList[2, 0], numberList[2, 1], numberList[2, 2]);
-            turns++;
+            Console.WriteLine("   |   |   \n {0} | {1} | {2} \n___|___|___\n   |   |   \n {3} | {4} | {5} \n___|___|___\n   |   |   \n {6} | {7} | {8} \n", _numberList[0, 0], _numberList[0, 1], _numberList[0, 2], _numberList[1, 0], _numberList[1, 1], _numberList[1, 2], _numberList[2, 0], _numberList[2, 1], _numberList[2, 2]);
         }
 
-        public static void CreateXorO(char playerSign, int input)
+        // Uses the int the player typed in to replace with an 'X' or 'O' in the array / playing field
+        public static void AddUserInputToArray(char addChar, int playerInput)
         {
-                 switch (input)
+            switch (playerInput)
             {
-                case 1: numberList[0, 0] = playerSign; break;
-                case 2: numberList[0, 1] = playerSign; break;
-                case 3: numberList[0, 2] = playerSign; break;
-                case 4: numberList[1, 0] = playerSign; break;
-                case 5: numberList[1, 1] = playerSign; break;
-                case 6: numberList[1, 2] = playerSign; break;
-                case 7: numberList[2, 0] = playerSign; break;
-                case 8: numberList[2, 1] = playerSign; break;
-                case 9: numberList[2, 2] = playerSign; break;
+                case 1: _numberList[0, 0] = addChar; break;
+                case 2: _numberList[0, 1] = addChar; break;
+                case 3: _numberList[0, 2] = addChar; break;
+                case 4: _numberList[1, 0] = addChar; break;
+                case 5: _numberList[1, 1] = addChar; break;
+                case 6: _numberList[1, 2] = addChar; break;
+                case 7: _numberList[2, 0] = addChar; break;
+                case 8: _numberList[2, 1] = addChar; break;
+                case 9: _numberList[2, 2] = addChar; break;
             }
-
+            _turns++;
         }
-
     }
 }
